754d3337e34b47b3aa7f2ac1bd3112a04b55d408
Alexey Galaev <o_0o_o@mail.ru>
2016-03-24 07:26:58 +0300

    Initial Commit

---
+++ GitClient/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GitClient
+//
+//  Created by Alexey Galaev on 24/03/16.
+//  Copyright © 2016 Alexey Galaev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
